47199c3dd9c161c1818718b076391f0eb22a133f
AlexanderGomes <gomes.alexsander.hamir@outlook.com>
2025-05-05 11:57:53 -0700

    generics design

--- memcontext/contextmanager.go
+++ memcontext/contextmanager.go
@@ -1,31 +1,58 @@
 package memcontext
 
+import (
+	"fmt"
+
+	"github.com/AlexsanderHamir/PoolX/src/pool"
+)
+
 func NewContextManager() *DefaultContextManager {
-	return &DefaultContextManager{}
+	return &DefaultContextManager{
+		contexts: make(map[string]any),
+	}
 }
 
-func (dcm *DefaultContextManager) CreateContext(name string) Context {
-	ctx := &DefaultContext{name: name}
-	dcm.contexts.Store(name, ctx)
+func CreateContext[T any](cm *DefaultContextManager, name string) *DefaultContext[T] {
+	ctx := &DefaultContext[T]{
+		ctxName: name,
+		pools:   make(map[string]*pool.Pool[T]),
+	}
+
+	cm.mu.Lock()
+	defer cm.mu.Unlock()
+	cm.contexts[name] = ctx
+
 	return ctx
 }
 
-func (dcm *DefaultContextManager) GetContext(name string) (Context, error) {
-	ctx, ok := dcm.contexts.Load(name)
+func GetContext[T any](dcm *DefaultContextManager, name string) (*DefaultContext[T], error) {
+	dcm.mu.RLock()
+	defer dcm.mu.RUnlock()
+
+	val, ok := dcm.contexts[name]
 	if !ok {
-		return nil, ErrContextNotFound
+		return nil, fmt.Errorf("context %s not found", name)
 	}
-	return ctx.(Context), nil
+
+	ctx, ok := val.(*DefaultContext[T])
+	if !ok {
+		return nil, fmt.Errorf("context %s has unexpected type", name)
+	}
+
+	return ctx, nil
 }
 
-func (dcm *DefaultContextManager) GetOrCreateContext(name string) (Context, error) {
-	ctx, err := dcm.GetContext(name)
+func GetOrCreateContext[T any](dcm *DefaultContextManager, name string) (*DefaultContext[T], error) {
+	ctx, err := GetContext[T](dcm, name)
 	if err != nil {
-		return dcm.CreateContext(name), nil
+		return CreateContext[T](dcm, name), nil
 	}
 	return ctx, nil
 }
 
 func (dcm *DefaultContextManager) DeleteContext(name string) {
-	dcm.contexts.Delete(name)
+	dcm.mu.Lock()
+	defer dcm.mu.Unlock()
+
+	delete(dcm.contexts, name)
 }
